75492f2cbac42083e759abfc75bc3321122ee42e
jorge1415802 <rjogeluis64@gmail.com>
2021-09-07 11:19:03 -0300

    add docs

---
+++ docs/precache-manifest.fae29f8316870bca925b5f963648dd5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3df56932046ab36a0840b64472ad4c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff38fbe3370e3ff0edc7",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff38fbe3370e3ff0edc7",
+    "url": "/static/js/main.a53a04fa.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
